e420c16c72c852592862b7f8dea1a9e9d3295b3c
narmuhamedov <kingradomir@gmail.com>
2026-01-27 18:54:33 +0600

    comment_def

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
+#функция вывода hello world
 def hello_world_view(request):
     if request.method == "GET":
         return HttpResponse("Hello World")
